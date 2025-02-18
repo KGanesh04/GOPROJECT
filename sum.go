4b4331e456c753185408489bebd1dfae67a1614c
KGanesh04 <89746331+KGanesh04@users.noreply.github.com>
2025-02-18 16:01:34 +0000

    pushing sum of two numbers

---
+++ sum.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+	num3 := 4
+	num4 := 5
+
+	fmt.Print("Enter first number: ")
+	fmt.Scan(&num1)
+
+	fmt.Print("Enter second number: ")
+	fmt.Scan(&num2)
+
+	sum := num1 + num2
+	sum1 := num3 + num4
+
+	fmt.Printf("The sum of %d and %d is: %d\n", num1, num2, sum)
+	fmt.Printf("sum of  num3:%d ,num4 :%d is %d\n", num3, num4, sum1)
+}
